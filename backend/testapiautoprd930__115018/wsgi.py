4c62e5b0123746d99163720bae8ea9e41f36d17d
Crowdbotics <engineering@crowdbotics.com>
2023-09-08 07:16:08 +0000

    Initial commit.

---
+++ backend/testapiautoprd930__115018/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testapiautoprd930__115018 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapiautoprd930__115018.settings')
+
+application = get_wsgi_application()
